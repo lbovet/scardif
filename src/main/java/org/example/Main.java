f38ef4ee1db9907c097600adc1cf6d19c68e1d1b
Laurent Bovet <laurent.bovet@windmaster.ch>
2024-03-07 12:58:15 +0100

    feat: add deps

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.networknt.schema.JsonMetaSchema;
+import com.networknt.schema.ValidationMessage;
+import com.networknt.schema.walk.JsonSchemaWalkListener;
+import com.networknt.schema.walk.WalkEvent;
+import com.networknt.schema.walk.WalkFlow;
+
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    private static class PropertiesKeywordListener implements JsonSchemaWalkListener {
+        @Override
+        public WalkFlow onWalkStart(WalkEvent keywordWalkEvent) {
+            JsonNode schemaNode = keywordWalkEvent.getSchemaNode();
+            schemaNode.get("title").textValue()
+            return WalkFlow.CONTINUE;
+        }
+
+        @Override
+        public void onWalkEnd(WalkEvent keywordWalkEvent, Set<ValidationMessage> validationMessages) {
+
+        }
+    }
+}
+
+}
