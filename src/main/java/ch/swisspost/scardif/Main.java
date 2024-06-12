d331550373c53728b08f891bc02309b2357c8fea
Laurent Bovet <laurent.bovet@windmaster.ch>
2024-06-12 23:57:07 +0200

    feat: generate graph

--- src/main/java/ch/swisspost/scardif/Main.java
+++ src/main/java/ch/swisspost/scardif/Main.java
@@ -1,5 +1,6 @@
 package ch.swisspost.scardif;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.networknt.schema.*;
 import com.networknt.schema.walk.JsonSchemaWalkListener;
 import com.networknt.schema.walk.WalkEvent;
@@ -7,20 +8,24 @@ import com.networknt.schema.walk.WalkFlow;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.base.CoreDatatype;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
 import org.eclipse.rdf4j.model.util.Values;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.Rio;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.*;
 
 public class Main {
 
-    public final static String jsonSchemaNs = "https://www.w3.org/2019/wot/json-schema#";
+    public final static String jsonSchemaNs = "https://jsonschema.org/";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         var schemaSource = Main.class.getClassLoader().getResourceAsStream("person-schema.json");
         var document = new Scanner(Main.class.getClassLoader().getResourceAsStream("john.json"), "UTF-8").useDelimiter("\\A").next();
@@ -37,13 +42,10 @@ public class Main {
         final JsonSchemaFactory schemaFactory = JsonSchemaFactory
                 .builder(JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012)).addMetaSchema(JsonMetaSchema.getV202012())
                 .build();
-        var schema = schemaFactory.getSchema(schemaSource, schemaValidatorsConfig);
+        var rootSchema = schemaFactory.getSchema(schemaSource, schemaValidatorsConfig);
+        rootSchema.walk(document, InputFormat.JSON, true);
 
-        schema.walk(document, InputFormat.JSON, true);
-
-        listener.getInstances().values().forEach(System.out::println);
-
-        Instance root = listener.instances.get("$");
+        listener.instances.values().forEach(System.out::println);
 
         ModelBuilder modelBuilder = new ModelBuilder().namedGraph(graphNs+instanceName);
 
@@ -56,9 +58,31 @@ public class Main {
             if("object".equals(instance.schema.properties.get("type"))) {
                 modelBuilder.add(instanceIri, RDF.TYPE, schemaIri(instance.schema.iri));
             }
+            if(instance.value != null) {
+                var format = instance.schema.properties.get("format");
+                var value = switch (instance.schema.properties.get("type").toString()) {
+                    case "number" -> Values.literal(instance.value.asDouble());
+                    case "integer" -> Values.literal(instance.value.asInt());
+                    case "boolean"-> Values.literal(instance.value.asBoolean());
+                    default -> format == null ? Values.literal(instance.value.asText()) :
+                        switch (format.toString()) {
+                            case "date" -> Values.literal(instance.value.asText(), CoreDatatype.XSD.DATE);
+                            case "date-time" -> Values.literal(instance.value.asText(), CoreDatatype.XSD.DATETIME);
+                            default -> Values.literal(instance.value.asText());
+                        };
+                };
+                modelBuilder.add(instanceIri, RDF.VALUE, value);
+                modelBuilder.add(instanceIri, RDFS.LABEL, instance.value.asText());
+            }
         });
 
-        RDFWriter writer = Rio.createWriter(RDFFormat.TRIG, System.out);
+        listener.getSchemas().values().forEach(schema -> {
+            schema.properties.forEach((property, value) -> {
+                modelBuilder.add(schemaIri(schema.iri), Values.iri(jsonSchemaNs+property), Values.literal(value));
+            });
+        });
+
+        RDFWriter writer = Rio.createWriter(RDFFormat.TRIG, new FileOutputStream("target/out.trig"));
         writer.startRDF();
         var model = modelBuilder.build();
         model.getNamespaces().forEach(ns -> writer.handleNamespace(ns.getPrefix(), ns.getName()));
@@ -85,6 +109,8 @@ public class Main {
 
         public String property;
 
+        public JsonNode value;
+
         public Schema schema;
 
         public String toString() {
@@ -99,7 +125,7 @@ public class Main {
     @RequiredArgsConstructor
     static class Schema {
         public final String iri;
-        public Map<String, String> properties = new HashMap<>();
+        public Map<String, Object> properties = new HashMap<>();
 
         public String toString() {
             return "  iri=" + iri + "\n" +
@@ -123,17 +149,26 @@ public class Main {
 
             var schemaLocation = event.getSchemaLocation().toString();
             var keyword = event.getKeyword();
-            var schemaIri = schemaLocation.replaceAll("/"+keyword+"$", "");
+            var schemaIri = schemaLocation.replaceAll("/"+keyword.replace("$", "\\$")+"$", "");
 
             Schema schema = schemas.computeIfAbsent(schemaIri, k -> new Schema(schemaIri));
 
-            var node = event.getSchemaNode().get(keyword);
-            if(node.isValueNode()) {
-                schema.properties.put(keyword, node.asText());
+            var schemaValue = event.getSchemaNode().get(keyword);
+            if(schemaValue.isValueNode() && !"$ref".equals(keyword)) {
+                schema.properties.put(keyword, switch (schemaValue.getNodeType()) {
+                    case NUMBER -> schemaValue.intValue();
+                    case BOOLEAN -> schemaValue.booleanValue();
+                    default -> schemaValue.textValue();
+                });
             }
 
-            if(schemaIri.endsWith("/$ref")) {
-                instance.property = schemaIri.replaceAll("/\\$ref$", "");
+            var instanceValue = event.getNode();
+            if(instanceValue.isValueNode()) {
+                instance.value = instanceValue;
+            }
+
+            if(schemaLocation.endsWith("/$ref")) {
+                instance.property = schemaIri;
             }
 
             var parent = event.getInstanceLocation().getParent();
